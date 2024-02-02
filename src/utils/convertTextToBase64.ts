19e630b11aafb47ea9c966f728455e8e6412464c
vvitenko-solvd <vvitenko@solvd.com>
2024-02-02 18:26:10 +0200

    - fix util import

--- src/utils/convertTextToBase64.ts
+++ src/utils/convertTextToBase64.ts
@@ -1,13 +1,13 @@
 import * as Base64 from "base64-js";
-import { isBrowser } from "./isBrowser.js";
-import { isMobile } from "./isMobile.js";
+import {isBrowser} from "./isBrowser.js";
+import {isMobile} from "./isMobile.js";
+import * as util from "util";
 
 let TextEncoder;
 
 if (isBrowser() && typeof window.TextEncoder === "function") {
   TextEncoder = window.TextEncoder;
 } else {
-  const util = require("util");
   TextEncoder = util.TextEncoder;
 }
 
@@ -18,7 +18,6 @@ export const convertTextToBase64 = (text: string) => {
     const encoder = new TextEncoder();
     const data = encoder.encode(text);
     const binary = new Uint8Array(data.buffer);
-    const base64 = Base64.fromByteArray(binary);
-    return base64;
+    return Base64.fromByteArray(binary);
   }
 };
