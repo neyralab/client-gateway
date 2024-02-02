4dca5cc2968584e7cc76945e04e84c24b2b381ac
fifa-master <107992228+fifa-master@users.noreply.github.com>
2024-02-02 16:48:05 +0200

    ovveride imports for es6

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
-export { encryptChunk } from "./encryptChunk";
-export { decryptChunk } from "./decryptChunk";
-export { sendChunk } from "./sendChunk";
-export { uploadFile, cancelingUpload } from "./uploadFile";
-export { downloadChunk } from "./downloadChunk";
-export { downloadFile } from "./downloadFile";
-export { swapChunk } from "./swapChunk";
-export { saveBlob } from "./saveBlob";
-export { countChunks } from "./countChunks";
-export { getUserRSAKeys } from "./getUserRSAKeys";
-export { publicKeyToPem } from "./publicKeyToPem";
-export { encodeExistingFile } from "./encodeExistingFile";
-export { LocalFileStream, LocalFileBuffer, LocalFileReactNativeStream } from "./types/File";
-export { getThumbnailImage, getThumbnailVideo } from "./getThumbnail";
+import { encryptChunk } from "./encryptChunk";
+import { decryptChunk } from "./decryptChunk";
+import { sendChunk } from "./sendChunk";
+import { uploadFile, cancelingUpload } from "./uploadFile";
+import { downloadChunk } from "./downloadChunk";
+import { downloadFile } from "./downloadFile";
+import { swapChunk } from "./swapChunk";
+import { saveBlob } from "./saveBlob";
+import { countChunks } from "./countChunks";
+import { getUserRSAKeys } from "./getUserRSAKeys";
+import { publicKeyToPem } from "./publicKeyToPem";
+import { encodeExistingFile } from "./encodeExistingFile";
+import { LocalFileStream, LocalFileBuffer, LocalFileReactNativeStream } from "./types/File";
+import { getThumbnailImage, getThumbnailVideo } from "./getThumbnail";
+
+export const pkg = {
+  encryptChunk,
+  decryptChunk,
+  sendChunk,
+  uploadFile, cancelingUpload,
+  downloadChunk,
+  downloadFile,
+  swapChunk,
+  saveBlob,
+  countChunks,
+  getUserRSAKeys,
+  publicKeyToPem,
+  encodeExistingFile,
+  LocalFileStream, LocalFileBuffer, LocalFileReactNativeStream,
+  getThumbnailImage, getThumbnailVideo
+}
