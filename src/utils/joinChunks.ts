8488127c0e9923a79d5456cc69817f93fedd26ca
SvirskyiAndrii <a.svirskyi@ghostdrive.com>
2024-09-02 15:24:13 +0300

    fix downloading files larger than 2gb

--- src/utils/joinChunks.ts
+++ src/utils/joinChunks.ts
@@ -1,7 +1,8 @@
 import { isBrowser } from './isBrowser.js';
+import { isMobile } from './isMobile.js';
 
 export const joinChunks = (chunks: ArrayBuffer[]) => {
-  if (isBrowser() && window.Blob) {
+  if (isMobile()) {
     return new Uint8Array(
       chunks.reduce((acc: Uint8Array, buffer) => {
         const tmpArray = new Uint8Array(buffer);
@@ -11,6 +12,8 @@ export const joinChunks = (chunks: ArrayBuffer[]) => {
         return newBuffer;
       }, new Uint8Array(0))
     );
+  } else if (isBrowser() && typeof window.Blob !== 'undefined') {
+    return new Blob(chunks);
   } else if (
     typeof process !== 'undefined' &&
     process.release.name === 'node'
