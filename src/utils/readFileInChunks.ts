9541d092cfcf8ec903462ad23471a950539c2b8c
SvirskyiAndrii <a.svirskyi@ghostdrive.com>
2024-09-03 10:16:07 +0300

    fixed readFileInChunks function

--- src/utils/readFileInChunks.ts
+++ src/utils/readFileInChunks.ts
@@ -3,27 +3,33 @@ import { LocalFileBuffer } from '../types/File/index.js';
 export async function* readFileInChunks(
   file: LocalFileBuffer,
   chunkSize: number
-): AsyncGenerator<ArrayBuffer> {
+): AsyncGenerator<Uint8Array> {
   const reader = file.stream().getReader();
+  let buffer = new Uint8Array();
 
   try {
     while (true) {
       const { done, value } = await reader.read();
       if (done) break;
 
-      let start = 0;
-      let end = Math.min(value.length, chunkSize);
+      const newSize = buffer.length + (value?.length || 0);
+      const newBuffer = new Uint8Array(newSize);
 
-      while (start < value.length) {
-        const slice = value.slice(start, end);
-        yield slice.buffer.slice(
-          slice.byteOffset,
-          slice.byteOffset + slice.byteLength
-        );
-
-        start = end;
-        end = Math.min(start + chunkSize, value.length);
+      newBuffer.set(buffer);
+      if (value) {
+        newBuffer.set(value, buffer.length);
       }
+
+      buffer = newBuffer;
+
+      while (buffer.length >= chunkSize) {
+        yield buffer.slice(0, chunkSize);
+        buffer = buffer.slice(chunkSize);
+      }
+    }
+
+    if (buffer.length > 0) {
+      yield buffer;
     }
   } catch (error) {
     console.error('Error reading file in chunks:', error);
