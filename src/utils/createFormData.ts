e9ca6b6b398a56048590d1c2a214141510f5d351
SvirskyiAndrii <a.svirskyi@ghostdrive.com>
2024-09-25 13:05:08 +0300

    fix createFormData for mobile

--- src/utils/createFormData.ts
+++ src/utils/createFormData.ts
@@ -1,4 +1,5 @@
 import { convertBase64ToUint8Array } from './convertBase64ToUint8Array.js';
+import { isMobile } from './isMobile.js';
 
 export const createFormData = (
   chunk: ArrayBuffer | string,
@@ -7,6 +8,16 @@ export const createFormData = (
 ): FormData => {
   const formData = new FormData();
   let data = chunk;
+
+  if (isMobile()) {
+    formData.append('file', {
+      uri: `data:${fileType};base64,${chunk}`,
+      type: fileType,
+      name: fileName,
+    } as any);
+
+    return formData;
+  }
 
   if (typeof chunk === 'string') {
     data = convertBase64ToUint8Array(chunk);
