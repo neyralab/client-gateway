5ec1e41cfce0e223cfb82c9ba74a749a2f7209cf
SvirskyiAndrii <a.svirskyi@ghostdrive.com>
2024-10-04 18:59:10 +0300

    turn off dts

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig([
   {
     entry: ['src/**/*.ts', '!src/**/*.test.ts'],
     format: ['esm'],
-    dts: true,
+    dts: false,
     sourcemap: false,
     clean: true,
     minify: false,
