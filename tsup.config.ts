a5cfcc44229d26fd90a8c0515a87b40099246030
SvirskyiAndrii <a.svirskyi@ghostdrive.com>
2024-10-07 10:42:47 +0300

    fix tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,15 +2,16 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig([
   {
-    entry: ['src/**/*.ts', '!src/**/*.test.ts'],
+    entry: ['src/index.ts', 'src/utils/**/*.ts'],
     format: ['esm'],
-    dts: false,
+    dts: true,
     sourcemap: false,
     clean: true,
     minify: false,
     splitting: false,
     target: 'esnext',
-    outDir: 'lib/esm',
+    outDir: 'lib/es6',
+    config: false,
     outExtension: () => ({ js: '.mjs' }),
   },
 ]);
